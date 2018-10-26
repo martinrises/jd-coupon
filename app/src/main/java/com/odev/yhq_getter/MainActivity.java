41523159ab151fb9661c7d3de73233b5f9a06f94
liuzhf <liuzhf@iskyu.com>
2018-10-26 14:14:47 +0800

    change TAG

--- app/src/main/java/com/odev/yhq_getter/MainActivity.java
+++ app/src/main/java/com/odev/yhq_getter/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String JSON_STR = "body=%7B%22activityId%22%3A%2241aqaSvcZnV7XDwDXm1NtiHiqz7J%22%2C%22from%22%3A%22H5node%22%2C%22scene%22%3A%221%22%2C%22args%22%3A%22key%3D297e27e0b46c4af69b2abe915a9ddf27%2CroleId%3D15012289%22%2C%22platform%22%3A%223%22%2C%22orgType%22%3A%222%22%2C%22openId%22%3A%22-1%22%2C%22pageClickKey%22%3A%22Babel_Coupon%22%2C%22eid%22%3A%22CQK7PEMEARUO45WX454C2XS2BSQFTVJ7LGHA7UQE3AHAGQIYCOVTVKNVSE5GGZ3GDXPSRLIHNLZ4XUYC3XE2ERPZ6U%22%2C%22fp%22%3A%226ab2735ff7bf092ac2ecd518b13c87cf%22%2C%22shshshfp%22%3A%22e1fe31b1ea1c14a949e93a2e1fc6dcf4%22%2C%22shshshfpa%22%3A%227c7ff8fb-4e5f-0226-66f1-789cd36ce324-1540483398%22%2C%22shshshfpb%22%3A%2214d6ce9f499d1479f949a60727c5877381393786c669488245bd1e9483%22%2C%22childActivityUrl%22%3A%22https%253A%252F%252Fpro.m.jd.com%252Fmall%252Factive%252F41aqaSvcZnV7XDwDXm1NtiHiqz7J%252Findex.html%253Futm_source%253Dpdappwakeupup_20170001%2526ShareTm%253DrChy88z654oe3N78RDkyVtbXv4u1BvFXsw2RbPGSSpOldf9IZcvqcSWYppbA84OiL98%25252FtP6Wa8KXIcNHUQVrTb8Y5Vh4bI%25252FPVO4%25252BDl59jTFjRF4FKUnjXC%25252F2yBEp3t6cOBS3CwTn%25252BawfYgoYTP9cpSH6nnqeEGX3twznhtfG%25252FBU%25253D%2526ad_od%253Dshare%2526utm_source%253Dandroidapp%2526utm_medium%253Dappshare%2526utm_campaign%253Dt_335139774%2526utm_term%253DWxfriends%2526from%253Dsinglemessage%22%2C%22mitemAddrId%22%3A%22%22%2C%22geo%22%3A%7B%22lng%22%3A%22%22%2C%22lat%22%3A%22%22%7D%2C%22addressId%22%3A%22%22%2C%22posLng%22%3A%22%22%2C%22posLat%22%3A%22%22%2C%22focus%22%3A%22%22%2C%22innerAnchor%22%3A%22%22%7D&client=wh5&clientVersion=1.0.0&sid=&uuid=1540483397320591160689&area=";
 
 
+    private static final String TAG = "yhq";
     private static final int MSG_YHQ = 96;
 
     private Handler mHandler;
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private long mStartTs, mIntervalMillis;
     private int mCnt, mSumMaxCnt, mCurrSumCnt;
     private Button mBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                                 mCurrSumCnt++;
                             }
                         } else {
-                            Log.i("yhq", "running diff >>> " + diff);
+                            Log.i(TAG, "running diff >>> " + diff);
                         }
                         synchronized (MainActivity.this) {
                             if (mIsRunning) {
@@ -93,12 +95,12 @@ public class MainActivity extends AppCompatActivity {
                         .subscribe(new Consumer<BaseResult>() {
                             @Override
                             public void accept(BaseResult baseResult) throws Exception {
-                                Log.i("doYhq", baseResult.getSubCodeMsg());
+                                Log.i(TAG, baseResult.getSubCodeMsg());
                             }
                         }, new Consumer<Throwable>() {
                             @Override
                             public void accept(Throwable throwable) throws Exception {
-                                Log.e("doYhq", throwable.toString());
+                                Log.e(TAG, throwable.toString());
                             }
                         });
             }
